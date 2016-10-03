7d3e70e25f543ec9199ddc416ce8bf7fef5a9f40
dovgych <g.dovgych@gmail.com>
2016-10-03 10:02:53 +0300

    Update module js file - changed http to https.

--- Module3_solution/narrow.module.js
+++ Module3_solution/narrow.module.js
@@ -2,5 +2,5 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.constant('MenuUrl', 'http://davids-restaurant.herokuapp.com/menu_items.json');
+.constant('MenuUrl', 'https://davids-restaurant.herokuapp.com/menu_items.json');
 })();
